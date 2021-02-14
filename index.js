217935ed8905defffb8ea36e52a90d55bcaceac1
Hyewon Baek <bbhyeone@gmail.com>
2021-02-14 04:15:38 -0800

    Revert "Set up a new project"

--- index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./src/app');
-
-const { log: print } = console;
-
-const port = 3000;
-
-app.listen(port, () => {
-	print(`This app listening at http://localhost:${port}`);
-});
