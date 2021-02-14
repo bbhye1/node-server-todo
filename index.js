1adab63afea57d14e98861e18f810c2d60b74386
Baek Hyewon <hyewon@Baeks-MacBook-Pro.local>
2021-02-14 22:32:28 +0900

    Github Actions
    Create ci file to run Github action
    Fix lint problem - indent

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const { log: print } = console;
 const port = 3000;
 
 app.listen(port, () => {
-	print(`This app listening at http://localhost:${port}`);
+  print(`This app listening at http://localhost:${port}`);
 });
--- src/app.test.js
+++ src/app.test.js
@@ -3,11 +3,12 @@ const request = require('supertest');
 const app = require('./app');
 
 describe('app', () => {
-	describe('GET /', () => {
-		it('response 200', async () => {
-			request(app)
-				.get('/')
-				.expect(200);
-		});
-	});
+  describe('GET /', () => {
+    it('responses 200', async (done) => {
+      request(app)
+        .get('/')
+        .expect(200)
+        .end(done);
+    });
+  });
 });
